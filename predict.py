efddb198e004a89f77a9f399fc6355f24c7f0227
AthArn <44187791+AthArn@users.noreply.github.com>
2018-10-17 09:18:44 +0530

    Update predict.py

--- predict.py
+++ predict.py
@@ -1,18 +1,12 @@
 import keras 
-import numpy as np
+import numpy as nm
 import pandas as pd
 model = keras.models.load_model(r'./models/model.06-0.09.hdf5') #give name of model in /models/ here
-
-test = np.genfromtxt(r'./data/test.csv',delimiter=',',skip_header=1,skip_footer = 0, usecols = range(0,784))
-test = np.array([data.reshape(28,28,1) for data in test])
-
-
+test = nm.genfromtxt(r'./data/test.csv',delimiter=',',skip_header=1,skip_footer = 0, usecols = range(0,784))
+test = nm.array([data.reshape(28,28,1) for data in test])
 pred = model.predict(test)
-pred = np.argmax(pred,axis=1)
+pred = nm.argmax(pred,axis=1)
 print(pred.shape)
-
-#probably don't need the following lines
-
 sample_submission_df = pd.read_csv(r'.\data\sample_submission.csv')
 my_submission_df = pd.DataFrame()
 my_submission_df['ImageId'] = sample_submission_df.ImageId
